9465dc10361a45ba1c44647b3adfc555529800ca
Jack SK8 PC <133342301@163.com>
2016-02-04 16:56:09 +1100

    Temp testing commit from PHPStorm

--- wp-config.php
+++ wp-config.php
@@ -20,8 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-//define( 'DB_NAME', 'sk8tech' );
-define( 'DB_NAME', 'skasia_wp1' );
+define( 'DB_NAME', 'sk8tech' );
+//define( 'DB_NAME', 'skasia_wp1' );
 
 /** MySQL database username */
 // define('DB_USER', 'root');
