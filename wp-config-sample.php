c1f02dabc64cfedbad0b8a2b1f5077d33ba0c975
133342301@163.com <Sk8Sk80826>
2016-11-13 23:38:41 +1100

    Updated repo

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'skasia_sk8tech');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'skasia_sk8tech');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password_here');
@@ -32,7 +32,7 @@ define('DB_PASSWORD', 'password_here');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
