0d5a2c4f9f583fda0d77a03070c6d8bff6f15f0b
Jacktator <133342301@163.com>
2016-12-27 19:58:00 +1100

    remove old theme

--- wp-content/themes/codeus/js/js_composer_inline.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-	vc_iframe.codeus_init_gallery = function ($element) {
-		$('.gallery', $element).each(function() {
-			$(this).on('noscript-loaded', codeus_init_gallery);
-		});
-	}
-
-})(jQuery);
