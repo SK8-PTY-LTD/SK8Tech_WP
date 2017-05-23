6ee9c3c28fe661445bc3f988d371901c8705a8ad
hi@sk8.tech <hi@sk8.tech>
2017-05-23 12:07:35 +0000

    [VP] Activated VersionPress 3.0.2
    VP-Action: versionpress/activate/3.0.2

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+[2017-03-16 00:08:08] ERROR: Form 2133 > MailChimp API error: 400 Bad Request. Your merge fields were invalid. 
+- FNAME : Please enter a value
+[2017-03-16 00:10:08] ERROR: Form 2133 > MailChimp API error: 400 Bad Request. Your merge fields were invalid. 
+- FNAME : Please enter a value
