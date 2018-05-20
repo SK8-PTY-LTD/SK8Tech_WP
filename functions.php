0ff83928244ef8fc6215c9fcab965be7544c8a63
jacktator <133342301@163.com>
2018-05-20 14:30:13 +0000

    Removed duplicated GA Tracking code

--- functions.php
+++ functions.php
@@ -60,48 +60,13 @@ function sdt_remove_ver_css_js($src) {
  * @author: Jack
  * @see: http://www.dezzain.com/snippets/how-to-fix-chinese-language-wordpress-excerpts-issue/
  */
-function dez_filter_chinese_excerpt( $output ) {
-global $post;
+function dez_filter_chinese_excerpt($output) {
+	global $post;
 //check if its chinese character input
-$chinese_output = preg_match_all("/\p{Han}+/u", $post->post_content, $matches);
-if($chinese_output) {
-$output = mb_substr( $output, 0, 50 ) . '...';
+	$chinese_output = preg_match_all("/\p{Han}+/u", $post->post_content, $matches);
+	if ($chinese_output) {
+		$output = mb_substr($output, 0, 50) . '...';
+	}
+	return $output;
 }
-return $output;
-}
-add_filter( 'get_the_excerpt', 'dez_filter_chinese_excerpt' );
-
-/**
- * CF7 + Google Analytics
- * @author Jack
- * @see https://contactform7.com/2017/06/07/on-sent-ok-is-deprecated/
- */
-add_action('wp_footer', 'cf7_ga_function');
-
-function cf7_ga_function() {
-	?>
-<script type="text/javascript">
-document.addEventListener( 'wpcf7mailsent', function( event ) {
-    if ( '316' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', 'Home Page');
-    } else if ( '265' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', 'Contact Page');
-    } else if ( '7726' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', 'Footer');
-    } else if ( '7727' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', '微信页面');
-    } else if ( '7750' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', 'WeChat Page');
-    } else if ( '7797' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', '主页');
-    }else if ( '7798' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', '网页开发');
-    }else if ( '7799' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', 'Web Design');
-    }else if ( '8265' == event.detail.contactFormId ) {
-        ga('send', 'event', 'CF7 Form', 'Submission', '微信小程序');
-    }
-}, false );
-</script>
-<?php
-}
+add_filter('get_the_excerpt', 'dez_filter_chinese_excerpt');
